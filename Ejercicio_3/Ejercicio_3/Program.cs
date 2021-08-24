ea05eb3a36b8134a17e52df90736e120fad467ad
BORIS NICHOLAS HERNANDEZ NUÑEZ <2021010562@cvirtualuees.edu.sv>
2021-08-24 08:42:55 -0600

    cambios

---
+++ Ejercicio_3/Ejercicio_3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ejercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad
+            
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+}
