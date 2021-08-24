6aadd04d8704980997c9a25ef6b5b8c4ee85561a
BORIS NICHOLAS HERNANDEZ NUÑEZ <2021010562@cvirtualuees.edu.sv>
2021-08-24 08:21:57 -0600

    cambios

--- Ejercicio_1/Ejercicio_1/Program.cs
+++ Ejercicio_1/Ejercicio_1/Program.cs
@@ -6,7 +6,14 @@ namespace Ejercicio_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hell a o World!");
+            double sueldo, btc;
+            
+            Console.WriteLine("Ingrese su sueldo en dolares USD: ");
+            sueldo = double.Parse(Console.ReadLine());
+
+            btc = (sueldo * 0.000020);
+
+            Console.WriteLine("Su sueldo en bitcoin es de: " + btc);
         }
     }
 }
