c1e8e9a002a65266c57e1c1cdf6fd711226fbad4
BORIS NICHOLAS HERNANDEZ NUÑEZ <2021010562@cvirtualuees.edu.sv>
2021-08-24 09:03:33 -0600

    cambios

--- Ejercicio_3/Ejercicio_3/Program.cs
+++ Ejercicio_3/Ejercicio_3/Program.cs
@@ -6,9 +6,30 @@ namespace Ejercicio_3
     {
         static void Main(string[] args)
         {
-            int edad
+            int edad, temp, cant;
+            string dpt, tos, fiebre;
             
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("********CONTROL DE SALUD*********");
+            Console.WriteLine("Ingrese la cantidad de empeados a evaluar");
+            cant = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= cant; i++)
+            {
+                Console.WriteLine();
+                Console.Write("Ingrese sus edad: ");
+                edad = int.Parse(Console.ReadLine());
+                Console.Write("Ingrese el area en el que labora ('Mercadeo', 'IT', 'Recursos Humanos'): ");
+                dpt = Console.ReadLine();
+                Console.Write("Ingrese su temperatura corporal: ");
+                temp = int.Parse(Console.ReadLine());
+                Console.Write("Presenta tos? (SI o NO): ");
+                tos = Console.ReadLine();
+                Console.Write("Presenta fiebre? (SI o NO): ");
+                fiebre = Console.ReadLine();
+
+                if ()
+
+            }
 
 
         }
