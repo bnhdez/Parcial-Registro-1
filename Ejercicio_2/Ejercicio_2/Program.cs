9c3671e38a1412095999b53ee3442440dfb2555c
BORIS NICHOLAS HERNANDEZ NUÑEZ <2021010562@cvirtualuees.edu.sv>
2021-08-24 08:36:22 -0600

    cambios

---
+++ Ejercicio_2/Ejercicio_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int años;
+            double sueldo, aumento, n_sueldo;
+
+            Console.Write("Ingrese los años de antigüedad: ");
+            años = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese sus sueldo: ");
+            sueldo = double.Parse(Console.ReadLine());
+
+            if (años == 5)
+            {
+                aumento = sueldo * 0.05;
+                n_sueldo = aumento + sueldo;
+                Console.WriteLine("Sus años en la empresa son " + años + ", contando con un bono de $" + aumento + " y un nuevo sueldo de $" + n_sueldo);
+            }
+
+        }
+    }
+}
